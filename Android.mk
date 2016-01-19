024cf2b3b78c19b449409bf22aba36722ebba4da
mrobbeloth-pdi <mrobbeloth@pdiarm.com>
2016-01-19 16:15:04 -0500

    Attempt to get past ckChangeLog dependency w/o success

--- Android.mk
+++ Android.mk
@@ -5,7 +5,10 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := $(call all-java-files-under)
 LOCAL_PACKAGE_NAME := com.uberspot.a2048
 LOCAL_STATIC_JAVA_LIBRARIES := de.cketti.library.changelog
+LOCAL_REQUIRED_MODULES := de.cketti.library.changelog
 LOCAL_MODULE_TAGS := optional
 LOCAL_CERTIFICATE := platform
+
+LOCAL_AAPT_FLAGS := --generate-dependencies --extra-packages de.cketti.library.changelog --auto-add-overlay
 
 include $(BUILD_PACKAGE)
